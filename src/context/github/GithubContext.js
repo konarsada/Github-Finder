3dd57cb4c7ce68b2d78f0e03687fbca9865c6046
konarsada <sadabratak@gmail.com>
2022-03-12 23:58:36 +0530

    422 error

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -35,6 +36,25 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    // Get single user
+    const getUser = async (login) => {
+        setLoading();
+
+        const response = await fetch(`${GITHUB_URL}/search/users/${login}`)
+
+        if(response.status === 404) {
+            window.location = '/notfound'
+        }
+        else {
+            const data = await response.json()
+        
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+    }
+
     // Clear users from state
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
@@ -44,8 +64,10 @@ export const GithubProvider = ({children}) => {
         <GithubContext.Provider value={{
             users: state.users,
             loading: state.loading,
+            user: state.user,
             searchUsers,
-            clearUsers
+            clearUsers,
+            getUser
         }}>
             {children}
         </GithubContext.Provider>
