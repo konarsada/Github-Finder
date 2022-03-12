0bbc048ee08fd206475cd2e48f27caecbe7dfa4a
konarsada <sadabratak@gmail.com>
2022-03-13 02:51:05 +0530

    User Profile and Alerts

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -40,7 +41,7 @@ export const GithubProvider = ({children}) => {
     const getUser = async (login) => {
         setLoading();
 
-        const response = await fetch(`${GITHUB_URL}/search/users/${login}`)
+        const response = await fetch(`${GITHUB_URL}/users/${login}`)
 
         if(response.status === 404) {
             window.location = '/notfound'
@@ -55,6 +56,25 @@ export const GithubProvider = ({children}) => {
         }
     }
 
+    // Get user repos
+    const getUserRepos = async (login) => {
+        setLoading();
+
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+
+        const data = await response.json()
+        
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data
+        })
+    }
+
     // Clear users from state
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
@@ -65,9 +85,11 @@ export const GithubProvider = ({children}) => {
             users: state.users,
             loading: state.loading,
             user: state.user,
+            repos: state.repos,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
         }}>
             {children}
         </GithubContext.Provider>
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -20,6 +20,13 @@ const githubReducer = (state, action) => {
                 loading: true
             }
         
+        case 'GET_REPOS':
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
+            }
+        
         case 'CLEAR_USERS':
             return {
                 ...state,
